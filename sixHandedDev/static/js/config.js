5e54f4f3239b4e699508087e10f182a57a66e7cf
root <ericjam@umich.edu>
2020-05-08 20:55:29 +0000

    touching up dev config

--- sixHandedDev/static/js/config.js
+++ sixHandedDev/static/js/config.js
@@ -1,9 +1,10 @@
-urlString = 'http://159.65.173.147:6543/';
+urlString = 'http://159.65.173.147:6541/';
 
 $(document).ready(function(){
-    $("#roomNumber").html("Room 1");
+    $("#roomNumber").html("DEV ENVIRONMENT");
 
-    var roomLinkString = '<div><a href="http://159.65.173.147:6542/">Go To Room 2</a></div>' +
+    var roomLinkString = '<div><a href="http://159.65.173.147:6543/">Go To Room 1</a></div>' +
+    '<div><a href="http://159.65.173.147:6542/">Go To Room 2</a></div>' +
     '<div><a href="http://159.65.173.147:3333/">Go To Room 3</a></div>' +
     '<div><a href="http://159.65.173.147:4444/">Go To Room 4</a></div>' +
     '<div><a href="http://159.65.173.147:5555/">Go To Room 5</a></div>' +
